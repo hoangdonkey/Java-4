631e69ea9a3e76ae636e43961ca1b31f17cf119e
hoangdonkey <hoangdonkey@users.noreply.github.com>
2021-03-05 21:20:36 +0700

    a >b ? "" : ""

--- GPA.java
+++ GPA.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
-public class API {
+public class GPA {
 	public static void main(String[] args) {
 		String name1, name2;
-		int math1, math2, eng1, eng2, sci1, sci2, API1, API2, sum1, sum2, result;
+		int math1, math2, eng1, eng2, sci1, sci2, GPA1, GPA2, sum1, sum2, result;
 		boolean compar;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter name of student 1: ");
@@ -40,23 +40,23 @@ public class API {
 		System.out.println("Science: " + sci2);
 
 		sum1 = math1 + eng1 + sci1;
-		API1 = sum1 * 100 / 300;
+		GPA1 = sum1 * 100 / 300;
 		sum2 = math2 + eng2 + sci2;
-		API2 = sum2 * 100 / 300;
+		GPA2 = sum2 * 100 / 300;
 		System.out.printf("\nTotal marks scored by %s: %d\n", name1, sum1);
-		System.out.printf("Precentage: %d%%\n", API1);
+		System.out.printf("Precentage: %d%%\n", GPA1);
 		System.out.printf("Total marks scored by %s: %d\n", name2, sum2);
-		System.out.printf("Precentage: %d%%\n\n", API2);
+		System.out.printf("Precentage: %d%%\n\n", GPA2);
 
-		System.out.printf("Is %s's percentage greater than %s? " + (API1 > API2 ? "Yes\n" : "No\n"), name1, name2);
-		System.out.printf("Is %s's percentage lesser than %s? " + (API1 < API2 ? "Yes\n" : "No\n"), name1, name2);
-		System.out.printf("Is %s's percentage equal to %s? " + (API1 == API2 ? "Yes\n" : "No\n"), name1, name2);
+		System.out.printf("Is %s's percentage greater than %s? " + (GPA1 > GPA2 ? "Yes\n" : "No\n"), name1, name2);
+		System.out.printf("Is %s's percentage lesser than %s? " + (GPA1 < GPA2 ? "Yes\n" : "No\n"), name1, name2);
+		System.out.printf("Is %s's percentage equal to %s? " + (GPA1 == GPA2 ? "Yes\n" : "No\n"), name1, name2);
 
-		if (API1 < 60) {
+		if (GPA1 < 60) {
 			result = 0;
 			System.out.printf("Scholarship Amount for %s is: $%d\n", name1, result);
 		}
-		else if (API1 >=60 && API1 < 75) {
+		else if (GPA1 >=60 && GPA1 < 75) {
 			result = 1000;
 			System.out.printf("Scholarship Amount for %s is: $%d\n", name1, result);
 		}
@@ -65,11 +65,11 @@ public class API {
 			System.out.printf("Scholarship Amount for %s is: $%d\n", name1, result);
 		}
 
-		if (API2 < 60) {
+		if (GPA2 < 60) {
 			result = 0;
 			System.out.printf("Scholarship Amount for %s is: $%d\n", name2, result);
 		}
-		else if (API2 >=60 && API2 < 75) {
+		else if (GPA2 >=60 && GPA2 < 75) {
 			result = 1000;
 			System.out.printf("Scholarship Amount for %s is: $%d\n", name2, result);
 		}
